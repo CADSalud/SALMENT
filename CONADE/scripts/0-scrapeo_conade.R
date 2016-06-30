6f1d96e9835e6e947a4b9d6d2e147219034e0420
Andreu Boada de Atela <andreu.boada@itam.mx>
2016-06-30 12:25:54 -0500

    reorganizacion

---
+++ CONADE/scripts/0-scrapeo_conade.R
@@ -0,0 +1,82 @@
+library(rvest)
+library(dplyr)
+library(parallel)
+
+obten_campo <- function(p,xpath){
+  p %>%
+    html_nodes(xpath = xpath) %>%
+    html_attr('value')
+}
+obten_checkbox <- function(p,xpath){
+  aux <- p %>%
+    html_nodes(xpath = xpath) %>%
+    html_attr('checked')
+  return(ifelse(is.na(aux),0,1))
+}
+
+xp_nombre <- '//*[@id="textfield"]'
+xp_lat1 <- '//*[@id="lat_grados"]'
+xp_lat2 <- '//*[@id="lat_minutos"]'
+xp_lat3 <- '//*[@id="lat_segundos"]'
+xp_lon1 <- '//*[@id="long_grados"]'
+xp_lon2 <- '//*[@id="long_minutos"]'
+xp_lon3 <- '//*[@id="long_segundos"]'
+xp_propietario <- '//*[@id="textfield4"]'
+xp_estado_uso <- '//*[@id="textfield5"]'
+xp_registrador <- '//*[@id="textfield13"]'
+xp_estado <- '//*[@id="textfield10"]'
+xp_municipio <- '//*[@id="textfield14"]'
+xp_calle <- '//*[@id="textfield15"]'
+xp_colonia <- '//*[@id="textfield23"]'
+xp_numext <- '//*[@id="textfield24"]'
+xp_numint <- '//*[@id="textfield25"]'
+xp_cp <- '//*[@id="textfield26"]'
+xp_checkenfermeria <- '//*[@id="checkbox"]'
+xp_checkguarderia <- '//*[@id="checkbox2"]'
+xp_checkestacionamiento <- '//*[@id="checkbox3"]'
+xp_checkescuela <- '//*[@id="checkbox4"]'
+xp_checkjuegos <- '//*[@id="checkbox5"]'
+
+obten_datos <- function(i){
+  pag <- read_html(paste0('http://sistemas.conade.gob.mx/portalCenso/instalacion_2.aspx?IDInstalacion=',i))
+  renglon <- data.frame(establecimiento = i,
+               nombre = obten_campo(pag,xp_nombre),
+               lat1 = obten_campo(pag,xp_lat1),
+               lat2 = obten_campo(pag,xp_lat2),
+               lat3 = obten_campo(pag,xp_lat3),
+               lon1 = obten_campo(pag,xp_lon1),
+               lon2 = obten_campo(pag,xp_lon2),
+               lon3 = obten_campo(pag,xp_lon3),
+               propietario = obten_campo(pag,xp_propietario),
+               estado_uso = obten_campo(pag,xp_estado_uso),
+               registrador = obten_campo(pag,xp_registrador),
+               estado = obten_campo(pag,xp_estado),
+               municipio = obten_campo(pag,xp_municipio),
+               calle = obten_campo(pag,xp_calle),
+               colonia = obten_campo(pag,xp_colonia),
+               numext = obten_campo(pag,xp_numext),
+               numint = obten_campo(pag,xp_numint),
+               cp = obten_campo(pag,xp_cp),
+               enfermeria = obten_checkbox(pag,xp_checkenfermeria),
+               guarderia = obten_checkbox(pag,xp_checkguarderia),
+               estacionamiento = obten_checkbox(pag,xp_checkestacionamiento),
+               escuela = obten_checkbox(pag,xp_checkescuela),
+               juegos = obten_checkbox(pag,xp_checkjuegos))
+  espacios_deportivos <- pag %>% html_nodes(css = '.Body') %>% html_text()
+  espacios_deportivos <- espacios_deportivos[7:length(espacios_deportivos)]
+  espacios_deportivos <- as.data.frame(matrix(espacios_deportivos,nrow=2,ncol=5,byrow = T))
+  colnames(espacios_deportivos) <- c('Cantidad','Espacio','Deporte Principal','Tipo','Aprovechamiento')
+  espacios_deportivos$establecimiento <- i
+  return(list(renglon,espacios_deportivos))
+}
+
+
+res1 <- mclapply(7863:19892, obten_datos, mc.cores = 4)
+
+res11 <- lapply(res1, function(x){return(x[[1]])})
+espacios_deportivos11 <- Reduce(bind_rows, res11)
+
+res21 <- lapply(res, function(x){return(x[[2]])})
+espacios_deportivos21 <- Reduce(bind_rows, res21)
+
+saveRDS(object = res11, file = '../output/res11.rds')
